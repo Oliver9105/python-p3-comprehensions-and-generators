eaefc5b386d3db9f883f084183fcfbb3360b971c
Oliver cheruiyot <olivercher1000@gmail.com>
2024-11-29 16:55:46 +0300

    Implement and test list comprehensions functions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [x for x in num_list if x % 2 == 0]
+    
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + '!' for sentence in sentence_list]
+
+if __name__ == "__main__":
+    
+    evens_result = return_evens([0, 1, 3, 5, 7, 8, 9])
+    print("Evens result:", evens_result) 
+    exclamation_result = make_exclamation(["Hello", "I'm doing great", "Python is fun"])
+    print("Exclamation result:", exclamation_result)  
+   
+
